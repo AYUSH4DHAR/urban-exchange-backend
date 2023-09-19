ed47a4178a20de99aa7a332418d9cfda85e6f043
Siddharths8212376 <siddharth8212376@gmail.com>
2023-09-19 15:20:15 +0530

    Changes - Dont show user data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,94 +5,93 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 userRouter.post("/", (req, res, next) => {
-    const User = new User({
-        name: req.body.name,
-        price: req.body.price,
-        email: req.body.email,
-        password: req.body.password,
-        username: req.body.username,
-        productsListed: req.body.productsListed ? req.body.productsListed : [],
-        productsPurchased: req.body.productsPurchased ? req.body.productsPurchased : [],
-        Phone: req.body.Phone ? req.body.Phone : '',
+  const User = new User({
+    name: req.body.name,
+    price: req.body.price,
+    email: req.body.email,
+    password: req.body.password,
+    username: req.body.username,
+    productsListed: req.body.productsListed ? req.body.productsListed : [],
+    productsPurchased: req.body.productsPurchased ? req.body.productsPurchased : [],
+    Phone: req.body.Phone ? req.body.Phone : '',
+  });
+  Users.save().then(createdPost => {
+    res.status(201).json({
+      message: "user added successfully",
+      postId: createdPost._id
     });
-    Users.save().then(createdPost => {
-        res.status(201).json({
-            message: "user added successfully",
-            postId: createdPost._id
-        });
-    });
+  });
 });
 userRouter.get("/", (req, res, next) => {
-    Users.find().then(documents => {
-        res.status(200).json({
-            message: "users fetched successfully!",
-            users: documents
-        });
+  Users.find().then(documents => {
+    res.status(200).json({
+      message: "users fetched successfully!",
+      users: documents
     });
+  });
 });
 userRouter.delete("/:id", (req, res, next) => {
-    Users.deleteOne({ _id: req.params.id }).then(result => {
-        console.log(result);
-        res.status(200).json({ message: "user deleted!" });
-    });
+  Users.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "user deleted!" });
+  });
 });
 
 userRouter.post("/signup", (req, res, next) => {
-    bcrypt.hash(req.body.password, 10).then(hash => {
-      const user = new Users({
-        email: req.body.email,
-        password: hash
-      });
-      user
-        .save()
-        .then(result => {
-          res.status(201).json({
-            message: "User created!",
-            result: result
-          });
-        })
-        .catch(err => {
-          res.status(500).json({
-            error: err
-          });
-        });
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new Users({
+      email: req.body.email,
+      password: hash
     });
-  });
-  
-  userRouter.post("/login", (req, res, next) => {
-    let fetchedUser;
-    Users.findOne({ email: req.body.email })
-      .then(user => {
-        if (!user) {
-          return res.status(401).json({
-            message: "Auth failed"
-          });
-        }
-        fetchedUser = user;
-        return bcrypt.compare(req.body.password, user.password);
-      })
+    user
+      .save()
       .then(result => {
-        if (!result) {
-          return res.status(401).json({
-            message: "Auth failed"
-          });
-        }
-        const token = jwt.sign(
-          { email: fetchedUser.email, userId: fetchedUser._id },
-          "secret_this_should_be_longer",
-          { expiresIn: "1h" }
-        );
-        res.status(200).json({
-          token: token,
-          expiresIn: 3600
+        res.status(201).json({
+          message: "User created!"
         });
       })
       .catch(err => {
-        return res.status(401).json({
-          message: "Auth failed"
+        res.status(500).json({
+          error: err
         });
       });
   });
+});
+
+userRouter.post("/login", (req, res, next) => {
+  let fetchedUser;
+  Users.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret_this_should_be_longer",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
+    });
+});
 
 
 module.exports = userRouter;
