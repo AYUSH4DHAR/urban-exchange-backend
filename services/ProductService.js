2b79e0b66011cf201863dbf8794b66413bb21b7e
Siddharths8212376 <siddharth8212376@gmail.com>
2024-05-24 17:48:36 +0530

    product service changes

--- services/ProductService.js
+++ services/ProductService.js
@@ -417,7 +417,7 @@ const getProductsByPageNoAndPageSizeAndOrCategory = async (req, res, next) => {
                 $geoNear: {
                     near: { type: "Point", coordinates: [Number(longitude), Number(latitude)] },
                     distanceField: "dist.calculated", // where the distance will be stored
-                    maxDistance: 10000, // 10 kilometers in meters
+                    maxDistance: 50000, // 10 kilometers in meters
                     spherical: true
                 }
             },
